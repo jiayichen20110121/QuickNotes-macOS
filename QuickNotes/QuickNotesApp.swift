f14beaa1209c38ce8dbda4e23d40c6e9e6f426dd
Jiayi <jiayichen20110121@126.com>
2025-11-07 11:43:00 +0800

    Initial Commit

---
+++ QuickNotes/QuickNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickNotesApp.swift
+//  QuickNotes
+//
+//  Created by cjy on 2025/11/7.
+//
+
+import SwiftUI
+
+@main
+struct QuickNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
